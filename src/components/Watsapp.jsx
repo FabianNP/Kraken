0f75fd20ff33016963ffc28dc142c25a3161b172
Fabian <burner.afire@gmail.com>
2024-01-13 17:57:28 -0600

    Whatsapp message changed

--- src/components/Watsapp.jsx
+++ src/components/Watsapp.jsx
@@ -4,7 +4,7 @@ import "./css/watsapp.css"
 const WatsappButton = ({ scroll }) => {
   return (
     <div className={`watsappButton ${scroll ?  "whatsappActive" : ""}`}>
-      <a href="https://wa.me/525512580055?text=Hola,20%buen20%dia.20%" target="_blank" >
+      <a href="https://wa.me/525512580055?text=Hola, buen dia." target="_blank" >
         <img src="whatsapp.png" alt="" />
       </a>
     </div>
